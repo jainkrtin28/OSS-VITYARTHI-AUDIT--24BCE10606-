7b5da248f024429efee345607a4e628df0386825
jainkrtin28 <232595090+jainkrtin28@users.noreply.github.com>
2026-03-31 00:06:15 +0530

    Add system identity report script

---
+++ script1.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# Script 1: System Identity Report
+# Author: Krtin Jain | Course: Open Source Software
+
+# --- Variables ---
+STUDENT_NAME="Krtin Jain"
+SOFTWARE_CHOICE="Git"
+
+# --- System Info ---
+KERNEL=$(uname -r)
+USER_NAME=$(whoami)
+UPTIME=$(uptime -p)
+DATE=$(date)
+DISTRO=$(lsb_release -d 2>/dev/null | cut -f2)
+
+# --- Display Output ---
+echo "======================================"
+echo " Open Source Audit — $STUDENT_NAME"
+echo " Software: $SOFTWARE_CHOICE"
+echo "======================================"
+echo "Distribution : $DISTRO"
+echo "Kernel       : $KERNEL"
+echo "User         : $USER_NAME"
+echo "Uptime       : $UPTIME"
+echo "Date & Time  : $DATE"
+echo "License      : GPL (Open Source Linux)"
+echo "======================================"
